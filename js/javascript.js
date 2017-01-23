5f1919a899c43a25ee034e28ef43cdbb3458f721
toowan <mail@toowan.me>
2017-01-23 15:04:37 -0800

    updated js

--- js/javascript.js
+++ js/javascript.js
@@ -14,44 +14,4 @@ $.getJSON('http://ip-api.com/json/?callback=?', function(data) {
   getWeather(city, state);
 });
 
-// using weather API and outputting results into our HTML
-function getWeather(city, state) {
-  var api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "" + fahrenheit + "" + appID;
 
-  $.ajax({
-    url: api,
-    dataType: 'json',
-    success: function(data) {
-      console.log(JSON.stringify(data));
-      // get condition of weather
-      var weatherCondition = data.weather[0].main;
-      // get weather icon
-      var weatherDayNight = data.weather[0].icon;
-      var weatherID = data.weather[0].id;
-      temp = {
-        F: Math.round((data.main.temp)) + "° F",
-        C: Math.round((data.main.temp - 32) * 5 / 9) + "° C"
-      };
-      $('.cityState').text(city + ", " +  state);
-      $('.data-nextTemp').html(temp['F']);
-      $('span').text(weatherCondition);
-
-
-    }
-  });
-}
-
-// Toggle between C and F
-  $('span').on('click', function() {
-    var current = $(this).data('nextTemp');
-    $('span').text(temp[current]);
-
-    if (current == 'C') {
-      $(this).data('nextTemp', 'F');
-      return;
-    }
-    $(this).data('nextTemp', 'C');
-  });
-
-
-});
