e4209834587fcd264e8e1fcac6cd5d0bd0d1d6a3
toowan <mail@toowan.me>
2017-01-23 14:12:03 -0800

    does this work

--- js/javascript.js
+++ js/javascript.js
@@ -24,7 +24,7 @@ function getWeather(city, state) {
     success: function(data) {
       console.log(JSON.stringify(data));
       // get condition of weather
-      var weatherOut = data.weather[0].main;
+      var weatherCondition = data.weather[0].main;
       // get weather icon
       var weatherDayNight = data.weather[0].icon;
       var weatherID = data.weather[0].id;
@@ -32,17 +32,16 @@ function getWeather(city, state) {
         F: Math.round((data.main.temp)) + "° F",
         C: Math.round((data.main.temp - 32) * 5 / 9) + "° C"
       };
-      $('#location').text(city + ", " +  state);
-      $('#temperature').html(temp['F']);
-      $('.weather').text(weatherOut);
+      $('.cityState').text(city + ", " +  state);
+      $('.data-nextTemp').html(temp['F']);
+      $('span').text(weatherCondition);
 
-      var pickURL = weatherCond[data.weather[0].icon].cond;
-      $('#weather-icon').css('background-image', 'url("' + pickURL + '")');
 
     }
   });
 }
 
+// Toggle between C and F
   $('span').on('click', function() {
     var current = $(this).data('nextTemp');
     $('span').text(temp[current]);
@@ -54,55 +53,5 @@ function getWeather(city, state) {
     $(this).data('nextTemp', 'C');
   });
 
-  var weatherCond = {};
-
-  weatherCond['01d'] = {
-    'cond': "http://wfiles.brothersoft.com/b/blue-sky-with-clear-water_175256-1600x1200.jpg"
-  };
-  weatherCond['01n'] = {
-    'cond': "https://web-assets.domo.com/blog/wp-content/uploads/2012/12/nighttimesky.jpeg"
-  };
-  weatherCond['03d'] = {
-    'cond': "http://icons.wunderground.com/data/wximagenew/1/1photogirl93/29-800.jpg"
-  };
-  weatherCond['03n'] = {
-    'cond': "http://icons.wunderground.com/data/wximagenew/1/1photogirl93/29-800.jpg"
-  };
-  weatherCond['04d'] = {
-    'cond': "http://icons.wunderground.com/data/wximagenew/1/1photogirl93/29-800.jpg"
-  };
-  weatherCond['03n'] = {
-    'cond': "http://icons.wunderground.com/data/wximagenew/1/1photogirl93/29-800.jpg"
-  };
-  weatherCond['09d'] = {
-    'cond': "http://www.pureella.com/wp-content/uploads/2011/04/pleasurefeelingpeoplephotographyarm.jpg"
-  };
-  weatherCond['09n'] = {
-    'cond': "http://alm0na.files.wordpress.com/2012/01/rain_drops_at_night-wide.jpg"
-  };
-  weatherCond['10d'] = {
-    'cond': "http://www.pureella.com/wp-content/uploads/2011/04/pleasurefeelingpeoplephotographyarm.jpg"
-  };
-  weatherCond['10n'] = {
-    'cond': "http://alm0na.files.wordpress.com/2012/01/rain_drops_at_night-wide.jpg"
-  };
-  weatherCond['11d'] = {
-    'cond': "http://www.prevention.com/sites/prevention.com/files/images/news/featured_images/thunderstorm-628x363-TS-135165621_0.jpg"
-  };
-  weatherCond['11n'] = {
-    'cond': "http://s3.amazonaws.com/rapgenius/1367403834_night-thunder-storm-lightning.jpg"
-  };
-  weatherCond['13d'] = {
-    'cond': "https://img.buzzfeed.com/buzzfeed-static/static/2014-01/enhanced/webdr07/24/6/anigif_enhanced-buzz-2717-1390562645-0.gif"
-  };
-  weatherCond['13n'] = {
-    'cond': "https://img.buzzfeed.com/buzzfeed-static/static/2014-01/enhanced/webdr07/24/6/anigif_enhanced-buzz-2717-1390562645-0.gif"
-  };
-  weatherCond['50d'] = {
-    'cond': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTNQzaAiOTFK70eqtdoaKTdMR63raFj-By0SklV6ixQPyTqt6_I"
-  };
-  weatherCond['50n'] = {
-    'cond': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTNQzaAiOTFK70eqtdoaKTdMR63raFj-By0SklV6ixQPyTqt6_I"
-  };
 
 });
